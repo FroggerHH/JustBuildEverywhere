39b5c4fae74d248609ea1f6e0378c06ba99078df
Vyacheslav <slava.shilin07@mail.ru>
2024-01-17 19:04:04 +0300

    _

--- Patch/BuildPatch.cs
+++ Patch/BuildPatch.cs
@@ -1,33 +1,18 @@
-﻿using BepInEx.Bootstrap;
-using HarmonyLib;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-using static JustBuildEverywhere.Plugin;
+﻿using HarmonyLib;
+using static Player.PlacementStatus;
 
 namespace JustBuildEverywhere;
 
 [HarmonyPatch]
 internal class BuildPatch
 {
-    [HarmonyPatch(typeof(ZNetScene), nameof(ZNetScene.Awake)), HarmonyPostfix]
-    public static void Patch(ZNetScene __instance)
+    [HarmonyPatch(typeof(Player), nameof(Player.UpdatePlacementGhost))] [HarmonyPostfix]
+    public static void Patch(Player __instance)
     {
-        foreach (var prefab in __instance.m_prefabs)
-        {
-            if(!prefab.TryGetComponent(out Piece piece)) continue;
-            
-            piece.m_groundOnly = false;
-            piece.m_canBeRemoved = true;
-            piece.m_allowedInDungeons = true;
-            piece.m_noInWater = false;
-            piece.m_notOnFloor = false;
-            piece.m_notOnWood = false;
-            piece.m_spaceRequirement = 0;
-            piece.m_vegetationGroundOnly = false;
-            piece.m_cultivatedGroundOnly = false;
-            piece.m_notOnTiltingSurface = false;
-            piece.m_inCeilingOnly = false;
-            piece.m_onlyInTeleportArea = false;
-        }
+        if (Player.m_localPlayer != __instance) return;
+        if (__instance.m_placementStatus == Invalid) return; 
+        if (!__instance.m_placementGhost) return;
+        __instance.m_placementStatus = Valid;
+        __instance.SetPlacementGhostValid(true);
     }
 }
--- Plugin.cs
+++ Plugin.cs
@@ -1,16 +1,20 @@
 ﻿using BepInEx;
-using BepInEx.Configuration;
 using HarmonyLib;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using UnityEngine;
 
 namespace JustBuildEverywhere;
 
 [BepInPlugin(ModGUID, ModName, ModVersion)]
 internal class Plugin : BaseUnityPlugin
 {
+    private void Awake()
+    {
+        _self = this;
+
+        harmony.PatchAll();
+
+        //1730069260
+    }
+
     #region values
 
     internal const string ModName = "JustBuildEverywhere", ModVersion = "1.1.0", ModGUID = "com.Frogger." + ModName;
@@ -19,49 +23,30 @@ internal class Plugin : BaseUnityPlugin
     internal static Plugin _self;
 
     #endregion
-    
+
     #region tools
 
     public static void Debug(string msg, bool localize = false)
     {
         if (Localization.instance != null && localize)
-        {
             _self.Logger.LogInfo(Localization.instance.Localize(msg));
-        }
         else
-        {
             _self.Logger.LogInfo(msg);
-        }
     }
 
     public void DebugError(string msg, bool showWriteToDev)
     {
-        if (showWriteToDev)
-        {
-            msg += "Write to the developer and moderator if this happens often.";
-        }
+        if (showWriteToDev) msg += "Write to the developer and moderator if this happens often.";
 
         Logger.LogError(msg);
     }
 
     public void DebugWarning(string msg, bool showWriteToDev)
     {
-        if (showWriteToDev)
-        {
-            msg += "Write to the developer and moderator if this happens often.";
-        }
+        if (showWriteToDev) msg += "Write to the developer and moderator if this happens often.";
 
         Logger.LogWarning(msg);
     }
 
     #endregion
-
-    private void Awake()
-    {
-        _self = this;
-        
-        harmony.PatchAll();
-        
-        //1730069260
-    }
 }
